bedc6aa0252d0d937a5b4ecb4a49ec452458cd89
Harsh Ruhil <ruhilharsh8@gmail.com>
2024-07-10 19:29:45 +0530

    Add files via upload
    here i have explained about how class methods can be used as an alternative to constructor functions in OOP.

---
+++ OOP2.py
@@ -0,0 +1,46 @@
+#  Let us learn to use classmethods as alternative constructors
+
+
+
+
+
+
+#Here we are creating an employee class
+class Employee:
+    increment=1.5
+
+    def __init__(self,fname,lname,salary):#constructor function
+        self.fname=fname
+        self.lname=lname
+        self.salary=salary
+    
+    def increase(self):
+        self.salary=self.salary*self.increment
+    
+    @classmethod#class methods are used to modify the code for a class,that is why they take the whole class as a parameter instead of taking self as a pareameter
+    def change_increment(cls,amount):
+        cls.increment=amount
+    
+    @classmethod
+    def from_str(cls,emp_string):
+        fname,lname,salary=emp_string.split("-")
+        return cls(fname,lname,salary)
+
+harsh=Employee("harsh","ruhil",50000000000000000)
+
+print(harsh.__dict__)#prints all the attributes of the instance in the form of a dictionary
+
+harsh.increment=10
+print(harsh.__dict__)
+
+print(Employee.__dict__)#prints all the attributes of employee class as a dictionary
+print(harsh.salary)
+
+
+Employee.change_increment(20)# the value of increment ghas changed for the whole "employee" class from 1.5 to 20 
+harsh.increase()
+
+print(harsh.salary)
+lovish=Employee.from_str("lovish-jackson-16000")
+
+print(lovish.fname)
